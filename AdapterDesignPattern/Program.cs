46c95ef9beefdc5356861b5c40369fbf034ad635
Akhilesh Anand <akhileshanand31@gmail.com>
2023-05-14 23:34:40 +0530

    added adapter design pattern

---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using AdapterDesignPattern.Adapter;
+using AdapterDesignPattern.Target;
+
+namespace AdapterDesignPattern
+{
+    public class Client
+    {
+
+        static void Main(string[] args)
+        {
+            IEmployee emp = new EmployeeAdapter();
+            string value = emp.GetEmployee();
+            Console.WriteLine(value);
+        }
+    }
+}
